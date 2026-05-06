672372ef594a82c5919572a352e0c903066d6dab
Yashraj <167480148+va-yash@users.noreply.github.com>
2026-05-06 23:16:17 +0200

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Roast-Me.me — Cosmic Damage Reports",
@@ -53,7 +54,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   );
 }
